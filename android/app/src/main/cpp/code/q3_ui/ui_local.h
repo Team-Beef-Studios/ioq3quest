4fab1c67dff60dd81acaeafa13622386624d6fbf
XQuader <xxquader@gmail.com>
2025-08-25 14:03:07 +0400

    Add "Exclude Bots" toggle in server browser (show real players number, if supported by server)

--- android/app/src/main/cpp/code/q3_ui/ui_local.h
+++ android/app/src/main/cpp/code/q3_ui/ui_local.h
@@ -67,6 +67,7 @@ extern vmCvar_t	ui_browserGameType;
 extern vmCvar_t	ui_browserSortKey;
 extern vmCvar_t	ui_browserShowFull;
 extern vmCvar_t	ui_browserShowEmpty;
+extern vmCvar_t	ui_browserExcludeBots;
 
 extern vmCvar_t	ui_brassTime;
 extern vmCvar_t	ui_drawCrosshair;
